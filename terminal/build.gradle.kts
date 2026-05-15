f2065596dc8344787b974bc35fc8213ac62ae17e
5337631 <5337631@qq.com>
2026-05-16 02:50:12 +0800

    Upload files batch 87

---
+++ terminal/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.ai.assistance.operit.terminal"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
